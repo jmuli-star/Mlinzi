e935ba8465ef3c42ed86ba2560847629cb1d22c4
kibetnathan <kibetnat@gmail.com>
2026-07-14 11:30:29 +0300

    feat(sorting): create a prefiltering function to get transactions less than a certain threshold

--- backend/sorting/prefilter.py
+++ backend/sorting/prefilter.py
@@ -28,9 +28,42 @@ def filter_min(df: pd.DataFrame, amount_col: str, min_value: float) -> pd.DataFr
         return df
 
 
+def filter_max(df: pd.DataFrame, amount_col: str, max_value: float) -> pd.DataFrame:
+    """
+    Filters DataFrame to only include transactions within maximum value
+    """
+    try:
+        if amount_col not in df.columns:
+            raise KeyError(f"column {amount_col} not found in the DataFrame")
+        df_clean = df.copy()
+        df_clean[amount_col] = pd.to_numeric(df_clean[amount_col], errors="coerce")
+
+        # filter out NaN and filter out maximum values
+        filtered_df = df_clean[df_clean[amount_col] <= max_value]
+
+        print(
+            f"Filtered out {len(df) - len(filtered_df)} transactions below {max_value}."
+        )
+        return filtered_df
+    except KeyError as e:
+        print(f"Filtering error: {e}")
+        return df
+
+    except Exception as e:
+        print(f"An unexpected error occurred during filtering: {e}")
+        return df
+
+
 def main():
     print(
         filter_min(
+            sorter.sort_csv("data/mlinzi_sample_transactions.csv", "customer_id"),
+            "amount_kes",
+            2000,
+        )
+    )
+    print(
+        filter_max(
             sorter.sort_csv("data/mlinzi_sample_transactions.csv", "customer_id"),
             "amount_kes",
             2000,
